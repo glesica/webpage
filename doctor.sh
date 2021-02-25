77d23bb1714f430b58589fbfab037f5b81b406ae
George Lesica <george@lesica.com>
2021-02-25 10:07:53 -0700

    Oops, fix doctor again

--- doctor.sh
+++ doctor.sh
@@ -4,13 +4,25 @@ set -e
 
 FAILED=0
 
-which make &> /dev/null || echo "make not found" && FAILED=1
-which pandoc &> /dev/null || echo "pandoc not found" && FAILED=1
-which python3 &> /dev/null || echo "python3 not found" && FAILED=1
+check() {
+    set +e
+    which "$1" &> /dev/null
+    set -e
+    if [[ $? -ne 0 ]]; then
+        echo "$1 not found"
+        FAILED=1
+    fi
+}
+
+check make
+check pandoc
+check python3
 
 if [[ $FAILED -ne 0 ]]; then
+    echo "some dependencies missing"
     exit 1
 fi
 
 echo "all dependencies satisfied"
+exit 0
 
