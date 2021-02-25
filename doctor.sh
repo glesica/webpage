9f8a0217704f66e0bd4e423048d091fe37393f49
George Lesica <george@lesica.com>
2021-02-25 09:47:43 -0700

    Improve doctor script

--- doctor.sh
+++ doctor.sh
@@ -2,7 +2,15 @@
 
 set -e
 
-which make &> /dev/null || echo "make not found"
-which pandoc &> /dev/null || echo "pandoc not found"
-which python3 &> /dev/null || echo "python3 not found"
+FAILED=0
+
+which make &> /dev/null || echo "make not found" && FAILED=1
+which pandoc &> /dev/null || echo "pandoc not found" && FAILED=1
+which python3 &> /dev/null || echo "python3 not found" && FAILED=1
+
+if [[ $FAILED -ne 0 ]]; then
+    exit 1
+fi
+
+echo "all dependencies satisfied"
 
